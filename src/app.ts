ea02f292eacb84d87f72d3ca392071eb4cb1b9c4
Chidike Henry <chidike.henry@gmail.com>
2025-03-30 17:21:05 +0100

    fix: restructoring files and folders

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,38 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import sequelize from "./config/db";
-import { server } from "./config/websocket"; // WebSocket server import
-import authroutes from "./routes/auth.routes";
+// import express from "express";
+// import cors from "cors";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import dotenv from "dotenv";
+// import sequelize from "./config/db";
+// import { server } from "./config/websocket"; // WebSocket server import
+// import authroutes from "./routes/auth.routes";
 
-// Load environment variables
-dotenv.config();
+// // Load environment variables
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-// Middleware
-app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// // Middleware
+// app.use(cors());
+// app.use(helmet());
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use("/api/auth", authroutes);
+// // Routes
+// app.use("/api/auth", authroutes);
 
-// Database Connection
-sequelize
-  .authenticate()
-  .then(() => console.log("✅ Database connected successfully."))
-  .catch((error: any) => console.error("❌ Database connection failed:", error));
+// // Database Connection
+// sequelize
+//   .authenticate()
+//   .then(() => console.log("✅ Database connected successfully."))
+//   .catch((error: any) => console.error("❌ Database connection failed:", error));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-// Start HTTP server (which includes WebSocket)
-server.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+// // Start HTTP server (which includes WebSocket)
+// server.listen(PORT, () => {
+//   console.log(`🚀 Server is running on http://localhost:${PORT}`);
+// });
 
-export default app;
+// export default app;
--- src/websocket/notification.event.ts
+++
@@ -1,5 +0,0 @@
-import { io } from "../config/websocket";
-
-export const sendNotification = (userId: string, message: string) => {
-  io.to(userId).emit("notification", { message });
-};
